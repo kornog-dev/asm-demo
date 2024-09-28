76c49bef504e70b99bc3b2d6019fd7c6de117e0e
Gaellan <doucet.mari@gmail.com>
2024-09-28 15:52:47 +0200

    api with basic commands and register

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# asm-demo
-An API and frontend app simulating an assembly and virtual machine used to teach the basics about assembly language and memory management.
+# Assembly Language Demonstration
+
+This is a teaching tool
+ to demonstrate basic principles of assembly language and memory management in low level programming.
+
+## The available registers
+
+### storage
+
+The storage register is designed to store values read or to be written in the memory.
+
+### condition
+
+The condition register is designed to store values to be used in verifying conditions.
+
+### cursor
+
+The cursor register is designed to store the current position of the memory cursor.
+
+### program
+
+The program register is designed to store the current program instruction number.
+
+
+## The assembly language
+
+### add
+
+`add [register] [value]` will add *value* to the specified *register* value. 
+
+### sub
+
+`add [register] [value]` will substract *value* to the specified *register* value.
+
+### get
+
+`get [register]` will store the value of the specified *register* in the storage register
+
+### set
+
+`set [register]` will store the value of the storage register in the specified *register*
+
+### read
+
+`read` will store the value stored in the memory at the current cursor position in the storage register.
+
+### write
+
+`write` will store the value stored in the storage register in the memory at the current cursor position .
+
+### jump
+
+`jump [instruction_number]` will set the program register value at the specified *instruction number*.
+
+### zjump
+
+`zjump [value] [instruction_number]` if the condition register contains the *value*, zjump will set the program register value at the specified *instruction number*.
+
---
+++ api/controllers/ApiController.php
@@ -0,0 +1,163 @@
+<?php
+
+class ApiController
+{
+    private array $memory;
+    private array $registers;
+
+    public function __construct()
+    {
+        if(isset($_SESSION["memory"])){
+            $this->memory = $_SESSION["memory"];
+        }
+        else
+        {
+            $this->memory = $this->initMemory();
+        }
+
+        if(isset($_SESSION["registers"])){
+            $this->registers = $_SESSION["registers"];
+        }
+        else
+        {
+            $this->registers = $this->initRegisters();
+        }
+    }
+
+    private function initMemory() : array
+    {
+        $memory = [];
+
+        for($i = 0; $i < 100; $i++)
+        {
+            $box = [
+                "id" => $i,
+                "value" => 0
+            ];
+            $memory[] = $box;
+        }
+
+        return $memory;
+    }
+
+    private function initRegisters() : array
+    {
+        $registers = [
+            "storage" => null,
+            "condition" => null,
+            "cursor" => null,
+            "program" => null,
+        ];
+
+        return $registers;
+    }
+
+    public function getAllInfos() : void
+    {
+        $_SESSION["registers"] = $this->registers;
+        $_SESSION["memory"] = $this->memory;
+
+        $data = [
+            "code" => 200,
+            "memory" => $this->memory,
+            "registers" => $this->registers,
+        ];
+
+        echo json_encode($data);
+    }
+
+    public function add(string $register, string $value) : void
+    {
+        $this->registers[$register] += intval($value);
+
+        $_SESSION["registers"] = $this->registers;
+
+        $this->getAllInfos();
+    }
+
+    public function sub(string $register, string $value) : void
+    {
+        $this->registers[$register] -= intval($value);
+
+        $this->getAllInfos();
+    }
+
+    public function get(string $register) : void
+    {
+        $this->registers["storage"] = $this->registers[$register];
+
+        $this->getAllInfos();
+    }
+
+    public function set(string $register) : void
+    {
+         $this->registers[$register] = $this->registers["storage"];
+
+        $this->getAllInfos();
+    }
+
+    public function read() : void
+    {
+        if(isset($this->memory[$this->registers["cursor"]]))
+        {
+            $this->registers["storage"] = $this->memory[$this->registers["cursor"]];
+
+            $this->getAllInfos();
+        }
+        else
+        {
+            $this->outOfBounds();
+        }
+    }
+
+    public function write() : void
+    {
+        if(isset($this->memory[$this->registers["cursor"]]))
+        {
+            $this->memory[$this->registers["cursor"]] = $this->registers["storage"];
+
+            $this->getAllInfos();
+        }
+        else
+        {
+            $this->outOfBounds();
+        }
+    }
+
+    public function jump(string $instructionNumber) : void
+    {
+        $this->registers["cursor"] = intval($instructionNumber);
+
+        $this->getAllInfos();
+    }
+
+    public function zjump(string $value, string $instructionNumber) : void
+    {
+        if($this->registers["condition"] === intval($value))
+        {
+            $this->registers["cursor"] = intval($instructionNumber);
+        }
+
+        $this->getAllInfos();
+    }
+
+    public function routeNotFound() : void
+    {
+        $data = [
+            "code" => 404,
+            "message" => "Unknown route"
+        ];
+
+        echo json_encode($data);
+    }
+
+    public function outOfBounds() : void
+    {
+        $data = [
+            "code" => 501,
+            "message" => "Cursor out of bounds"
+        ];
+
+        echo json_encode($data);
+    }
+}
