08c1130e254cb7a2cc1d909e4b0688cb9aaef384
Gaellan <doucet.mari@gmail.com>
2025-10-01 11:51:30 +0200

    init token in app

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,8 +1,19 @@
-import {initMemory} from "./memory.js";
+import {initMemory, updateMemory} from "./memory.js";
 import {loadProgram, runProgram} from "./program.js";
 
 window.addEventListener("DOMContentLoaded", function(){
     initMemory();
+
+    let params = new URLSearchParams({
+        route : 'token'
+    });
+    fetch("http://asm-demo.kornog-formations.com/api/index.php?" + params.toString()).then(
+        (response) => {
+            response.json().then((data) => {
+                localStorage.setItem("token", data.token);
+            })
+        }
+    );
 
     let loadProgramForm = document.querySelector("#load-program");
     loadProgramForm.addEventListener("submit", loadProgram);
